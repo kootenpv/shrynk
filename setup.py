9fbda4bfaf7a96f1c99a47b8a7f627705bd3ef8a
Pascal Vankooten <pascal.vankooten@leaseplan.com>
2019-12-05 21:31:51 +0100

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 MAJOR_VERSION = '0'
 MINOR_VERSION = '2'
-MICRO_VERSION = '19'
+MICRO_VERSION = '21'
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(
